a9d5a3cee618056e23dbe907cc1c0577bd8d864c
Daniel-Williams-ux <williamsoligodaniel@gmail.com>
2023-04-30 12:33:24 +0000

    Listen for new connections and messages

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ const server = http.Server(app);
 //Extend the server with Socket.io
 const sockets = require("socket.io");
 io = sockets(server);
+
+//Listen for new connections and messages
+io.on("connection", function (connection) {
+  connection.on("message", function (data) {
+    console.log("new message: " + data);
+    io.emit("broadcast", data);
+  });
+});
